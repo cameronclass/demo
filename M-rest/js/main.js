da1ae904a5c0e9a4279aab063bec76eeda93a2b2
Cameron Class <kamrankutumov@gmail.com>
2020-11-27 04:50:47 +0300

    initial commit

--- M-rest/js/main.js
+++ M-rest/js/main.js
@@ -1,7 +1,25 @@
 // Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+/* $(function () {
+  var $preloader = $("#page-preloader"),
+    $spinner = $preloader.find(".spinner");
+  $spinner.fadeOut();
+  $preloader.delay(350).fadeOut("slow");
+}); */
+
+var galleryThumbs = new Swiper(".home-finished .gallery-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: false,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper(".home-finished .gallery-top", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
 });
