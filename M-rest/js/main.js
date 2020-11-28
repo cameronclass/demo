b8bab611ea2de52e150f077a71d2922a866bb7a5
Cameron Class <kamrankutumov@gmail.com>
2020-11-28 03:15:49 +0300

    initial commit

--- M-rest/js/main.js
+++ M-rest/js/main.js
@@ -6,19 +6,46 @@
   $preloader.delay(350).fadeOut("slow");
 }); */
 
-var galleryThumbs = new Swiper(".home-finished .gallery-thumbs", {
+$(function () {
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".slider-nav",
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    focusOnSelect: true,
+  });
+});
+
+var swiper = new Swiper(".swiper-container-upside", {
+  nested: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".home-finished .swiper-next",
+    prevEl: ".home-finished .swiper-prev",
+  },
+});
+
+var galleryThumbs = new Swiper(".gallery-thumbs", {
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: false,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  nested: true,
 });
-var galleryTop = new Swiper(".home-finished .gallery-top", {
+
+var galleryTop = new Swiper(".gallery-top", {
   spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  nested: true,
   thumbs: {
     swiper: galleryThumbs,
   },
